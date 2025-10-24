f864de8bd9abfc9f051c1d7cce6959c34fb261de
unknown <kim0615157@gmail.com>
2025-10-24 18:07:43 +0900

    refactor: 객체의 역할 분리
    RacingcarController에 있던 playTurn, findWinnerCarNames, getMaxDistance에 있던 메서드를 RacingCars로 이동
    
    RacingCars가 자동차 레이싱, 우승자 찾기의 책임을 갖도록 리팩토링 하였음

--- src/main/java/racingcar/controller/RacingcarContoller.java
+++ src/main/java/racingcar/controller/RacingcarContoller.java
@@ -1,10 +1,8 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
-import racingcar.model.RacingCars;
 import racingcar.model.Car;
+import racingcar.model.RacingCars;
 import racingcar.view.RacingcarInputView;
 import racingcar.view.RacingcarOutputView;
 
@@ -15,47 +13,16 @@ public class RacingcarContoller {
         int attemptCount = RacingcarInputView.attemptCountInput();
 
         RacingCars racingCars = new RacingCars(carNames);
-        List<Car> cars = racingCars.getRacingCars();
 
         RacingcarOutputView.printExecutionResult();
         for (int i=0; i<attemptCount; i++) {
-            playTurn(cars);
-            RacingcarOutputView.printProgress(cars);
+            racingCars.playTurn();
+
+            List<Car> turnCars = racingCars.getRacingCars();
+            RacingcarOutputView.printProgress(turnCars);
         }
 
-        List<String> winnerCarNames = getWinners(cars);
+        List<String> winnerCarNames = racingCars.findWinnerCarNames();
         RacingcarOutputView.printWinners(winnerCarNames);
-    }
-
-    private void playTurn(List<Car> cars) {
-        for (Car car : cars) {
-            int randomNumber = Randoms.pickNumberInRange(0, 9);
-            car.move(randomNumber);
-        }
-    }
-
-    private List<String> getWinners(List<Car> cars) {
-        int maxDistance = getMaxDistance(cars);
-        List<String> winnerCarNames = new ArrayList<>();
-
-        for (Car car : cars) {
-            if (car.getDistance() == maxDistance) {
-                winnerCarNames.add(car.getCarName());
-            }
-        }
-
-        return winnerCarNames;
-    }
-
-    private int getMaxDistance(List<Car> cars) {
-        int maxDistance = 0;
-
-        for (Car car : cars) {
-            if (car.getDistance() > maxDistance) {
-                maxDistance = car.getDistance();
-            }
-        }
-
-        return maxDistance;
     }
 }
--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,22 +1,55 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
 
-    private List<Car> racingCars;
+    private List<Car> cars;
 
     public RacingCars(List<String> carNames) {
-        List<Car> racingCars = new ArrayList<>();
+        List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
-            racingCars.add(new Car(carName));
+            cars.add(new Car(carName));
         }
 
-        this.racingCars = racingCars;
+        this.cars = cars;
     }
 
     public List<Car> getRacingCars() {
-        return racingCars;
+        return cars;
+    }
+
+    public void playTurn() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            car.move(randomNumber);
+        }
+    }
+
+    public List<String> findWinnerCarNames() {
+        int maxDistance = getMaxDistance();
+        List<String> winnerCarNames = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getDistance() == maxDistance) {
+                winnerCarNames.add(car.getCarName());
+            }
+        }
+
+        return winnerCarNames;
+    }
+
+    private int getMaxDistance() {
+        int maxDistance = 0;
+
+        for (Car car : cars) {
+            if (car.getDistance() > maxDistance) {
+                maxDistance = car.getDistance();
+            }
+        }
+
+        return maxDistance;
     }
 }
