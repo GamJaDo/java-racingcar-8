0ec121141f0ad6ce3a981afc04402c01c95fb225
unknown <kim0615157@gmail.com>
2025-10-25 10:31:07 +0900

    test:구현한 프로그램의 예상결과가 맞는지 테스트 코드를 작성함
    기능_테스트_1인_우승일_경우, 기능_테스트_2인_우승일_경우 2개의 테스트 메서드를 작성 후 입력한 결과에 대해 예상 결과값이 맞는지 테스트 코드 작성 후 확인
    
    예외_테스트_자동차이름_5글자_초과, 예외_테스트_자동차이름_문자열에_구분자_쉼표가_없음, 예외_테스트_이동횟수_입력시_숫자외에_입력할_경우 메서드를 작성 후 hasMessage를 이용하여 예상 예외 처리 메시지가 정확히 맞는지 확인

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -9,11 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    void 기능_테스트_1인_우승일_경우() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
@@ -24,10 +25,44 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 기능_테스트_2인_우승일_경우() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni,jun", "3");
+                assertThat(output()).contains("pobi : ", "woni : -", "jun : -",
+                    "pobi : -", "woni : --", "jun : --", "pobi : -", "woni : ---", "jun : ---",
+                    "최종 우승자 : woni, jun");
+            },
+            STOP, MOVING_FORWARD, MOVING_FORWARD,
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+            STOP, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차이름_5글자_초과() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+            assertThatThrownBy(() -> runException("pobi,wonijun", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5자 이하만 가능합니다.")
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차이름_문자열에_구분자_쉼표가_없음() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobiwonijun", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구분자 쉼표(,)가 존재하지 않습니다.")
+        );
+    }
+
+    @Test
+    void 예외_테스트_이동횟수_입력시_숫자외에_입력할_경우() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni,jun", "r"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도 횟수는 숫자만 입력해야 합니다.")
         );
     }
 
